310cb527f69eded0716b9e990d6041ee9e47f773
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-12 21:40:19 +0300

    Create 0010-array-traits2.py

---
+++ 0010-array-traits2.py
@@ -0,0 +1,25 @@
+c = np.random.randint(5, size = (4, 6))
+
+print(c)
+
+"""
+[[3 4 1 3 2 4]
+ [2 0 0 2 2 1]
+ [0 2 3 3 2 4]
+ [2 4 2 2 4 4]]
+"""
+
+print(c.ndim) 
+
+print(c.shape) 
+
+print(c.dtype) 
+
+print(c.size) 
+
+"""
+2
+(4, 6)
+int32
+24
+"""
