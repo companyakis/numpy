659b216dffd41e3fae5a501f68997f3136423449
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-18 14:35:33 +0300

    Create 0020-math3.py

---
+++ 0020-math3.py
@@ -0,0 +1,36 @@
+a = np.arange(8)
+
+print(a)
+
+print(np.add(a, 1))
+
+print(np.subtract(a, 2))
+
+print(np.multiply(a, 3))
+
+print(np.divide(a, 2))
+
+print(np.power(a, 2))
+
+print(np.mod(a, 4))
+
+print(np.absolute(np.subtract(a, 15)))
+
+"""
+[0 1 2 3 4 5 6 7]
+
+[1 2 3 4 5 6 7 8]
+
+[-2 -1  0  1  2  3  4  5]
+
+[ 0  3  6  9 12 15 18 21]
+
+[0.  0.5 1.  1.5 2.  2.5 3.  3.5]
+
+[ 0  1  4  9 16 25 36 49]
+
+[0 1 2 3 0 1 2 3]
+
+[15 14 13 12 11 10  9  8]
+
+"""
