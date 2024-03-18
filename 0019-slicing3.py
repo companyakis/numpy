b951487d111d5ad336a524a147885c49e8fa4273
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-18 14:22:37 +0300

    Create 0019-slicing3.py

---
+++ 0019-slicing3.py
@@ -0,0 +1,21 @@
+np.random.seed(12)
+
+a = np.random.randint(15, size = (6, 5)) # elements between 0 and 15 and 6 * 5 dimemnsion
+
+print(a)
+
+print("......................")
+
+print(a[:, 0]) #all rows and first column
+
+"""
+[[11 11  6 13  1]
+ [ 2  3  3 12  0]
+ [ 6  1  4  5 13]
+ [ 9  2 11  6 10]
+ [ 0  5  8 12 13]
+ [ 2  9  3 14  4]]
+......................
+[11  2  6  9  0  2]
+
+"""
