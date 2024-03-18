b9ad9d24ca96435b49a7f0514ecafdc864d9fd0f
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-18 14:38:48 +0300

    Create 0021-statistics1.py

---
+++ 0021-statistics1.py
@@ -0,0 +1,11 @@
+a = np.arange(-3, 10)
+
+print(a) # [-3 -2 -1  0  1  2  3  4  5  6  7  8  9]
+
+print(np.mean(a)) # 3
+
+print(np.sum(a) / len(a)) # 3
+
+print(np.max(a)) # 9
+
+print(np.min(a)) # -3
